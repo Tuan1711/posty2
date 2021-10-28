af97e4b674308a3b4149f9436e2b1e6c0fe1900d
Tuan <nguyendinhtuan17112001@gmail.com>
2021-10-28 14:41:11 +0700

    auth for like and unlike button

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(4);
+        $posts = Post::with(['user', 'likes'])->paginate(4);
         return view('posts.index', [
             'posts' => $posts,
         ]);
