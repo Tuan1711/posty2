25839d0fc1b8bc2e7fade9c5571fddcca374c9cd
Tuan <nguyendinhtuan17112001@gmail.com>
2021-10-28 18:14:32 +0700

    Single post page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,4 +33,11 @@ class PostController extends Controller
         $post->delete();
         return back();
     }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
 }
